623f8b44c8752df8d3dd5c5b8356c806db8159a4
최한빈 <gksqlsl11@khu.ac.kr>
2022-06-28 16:30:02 +0900

    Design floating Contents in Intro

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -9,6 +9,8 @@ import thumbup from '../../img/thumbup.png';
 import Crown from '../../img/crown.png';
 import Glassesimoji from '../../img/glassesimoji.png';
 import Profile from '../../img/profile.png';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
+
 
 const Intro = () => {
     return (
@@ -34,6 +36,23 @@ const Intro = () => {
             <img src= {Vector1} alt = "" /> 
             <img src= {Vector2} alt = "" />
             <img src= {Profile} alt = "" /> 
+            <img src= {Glassesimoji} alt = "" /> 
+            <div style={{top: '-4%', left: '50%'}}>
+                <FloatingDiv image = {Crown} txt1='Backend' txt2 = 'Developer' />
+            </div>
+            <div style={{top: '18rem', left: '0rem'}}>
+                <FloatingDiv image = {thumbup} txt1='Python' txt2 = 'Instructor' />
+            </div>
+
+            {/* blur divs */}
+            <div className='blur' style= {{background:'rgb(238 210 255)'}}></div>
+            <div className='blur' style= {{
+                                        background:'#C1F5FF', 
+                                        top: '17rem',
+                                        width: '21rem',
+                                        height:'11rem',
+                                        left:'-9rem'
+                                        }}></div>
             
         </div>
     </div>
