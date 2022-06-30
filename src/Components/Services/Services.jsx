48363d0030d25954aea3ee298566ace47a7e0dc6
최한빈 <gksqlsl11@khu.ac.kr>
2022-06-30 20:33:36 +0900

    Design right side at services

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,6 +3,7 @@ import './Services.css'
 import HeartEmoji from "../../img/heartemoji.png";
 import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
+import Card from '../Card/Card'
 
 const Services = () => {
   return (
@@ -11,14 +12,40 @@ const Services = () => {
         <div className='awesome'>
             <span>My Awesome</span>
             <span>services</span>
-            <spane>내용 적기</spane>
+            <span>내용 적기</span>
+
+            {/* resume는 a태그로 감싸자*/}
             <button className='button s-button'>Download CV</button>
             <div className='blur s-blurl' style= {{background: "#ABF1GG94"}}></div>
         </div>
     
         {/* right side */}
         <div className='cards'>
-            I am the right side
+            <div style={{left: '14rem'}}>
+              <Card
+                emoji = {HeartEmoji}
+                heading = {'Developer'}
+                detail = {'stack'}
+              />
+            </div>
+
+            {/* Second card */}
+            <div style={{top : '10rem', left: '-4rem'}}>
+              <Card
+                emoji = {Glasses}
+                heading = {'프로젝트?'}
+                detail = {'stack'}
+              />
+            </div>
+
+            {/* Third card */}
+            <div style={{top: '19rem', left: '11rem'}}>
+              <Card
+                emoji = {Humble}
+                heading = {'그 외 경험'}
+                detail = {'stack'}
+              />
+            </div>
         </div>
     </div>
   )
